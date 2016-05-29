fb08a631982288a996a72f068f29a403b593039e
Brandon Richards <bbtb1982@gmail.com>
2016-05-28 19:31:07 -0600

    moved utils.js to filters.js and updated tests

--- test/utils/filters-test.js
+++ test/utils/filters-test.js
@@ -1,29 +1,27 @@
-import { parse_model, parse_model_1 } from '../app/utils/utils.js'
-import { parse_values } from '../app/utils/utils.js'
-import { parse_filters } from '../app/utils/utils.js'
+import { parse_model, parse_model_1, parse_values, parse_filters } from '../../app/utils/filters.js'
 var expect = require('chai').expect
 
 describe('Array', function() {
-  describe('filter[post]=1,2,3', function () {
-    it('it should return post from string', function () {
+  describe('utils/filters.js tests', function () {
+    it(' spike test regex filtering:: it should return post from string', function () {
       var queryParam = "filter[post]=1,2,3";
       var model = queryParam.match(/[^filter=\[].[^\]]*/gi)[0]
       expect(model).to.equal('post');
     });
 
-    it('it should return post from string', function () {
+    it('parse_model():: it should return post from string', function () {
       expect(parse_model("filter[post]=1,2,3")).equal('post');
     });
 
-    it('parse_model_1() should return the model key', function () {
+    it('parse_model_1():: parse_model_1() should return the model key', function () {
       expect(parse_model_1("filter[post]=1,2,3")).equal('post');
     });
 
-    it('parse_vaules should return an array of ids', function(){
+    it('parse_values():: parse_vaules should return an array of ids', function(){
       expect(parse_values("filter[post]=1,2,3")).to.deep.equal([1,2,3])
     })
 
-    it('it should parse multiple filter query Params', function () {
+    it('parse_filters()::  it should parse multiple filter query Params', function () {
       var params = parse_filters("filter[post]=1,2,3&filter[tag]=3,2,1")
       expect(params).to.have.all.keys('post', 'tag');
       expect(params['post']).to.deep.equal([1,2,3]);
